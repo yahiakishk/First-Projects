e52848ef40ef2f83fdcb8357e4df7c3fb011288d
Yahia Kishk <76238372+yahiakishk@users.noreply.github.com>
2025-09-26 16:36:38 +0200

    Update and rename madlibs game to madlibs game.py

--- madlibs game.py
+++ madlibs game.py
@@ -6,8 +6,6 @@ emotion1 = input("Enter an emotion: ")
 adjective3 = input("Enter yet another adjective: ")
 
 
-
-
 print(f"Today I to went a {adjective1} zoo")
 print(f"In one of the exhibits I saw a {noun1}")
 print(f"The {noun1} was {verb1} a {adjective2}")
